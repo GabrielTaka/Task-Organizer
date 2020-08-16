379208f0a198fed6b9cfd47988cc3b094b43744f
Gabriel <gabriel.vargas@continuodev.com.br>
2020-08-15 21:00:31 -0300

    add component task

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tasks">
+    <template v-if="tasks.length">
+      <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+    </template>
+    <template v-else>
+      <p class="no-task"> Sua vida está em dia :) </p>
+    </template>
+  </div>
+</template>
+
+<script>
+  import Task from "@/components/Task"
+
+  export default {
+    components: { Task },
+    props: {
+      tasks: { type: Array, required: true }
+    }
+  }
+</script>
+
+<style scoped>
+  .tasks {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .tasks .task {
+    margin: 10px;
+  }
+
+  .no-task {
+    color: #AAA;
+    font-size: 1.7rem;
+  }
+</style>
